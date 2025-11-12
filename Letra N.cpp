e0ea7b4739ea81eba4df451bc4371eb663acaec1
AngelinGB <gonzalezjohangi1@gmail.com>
2025-11-12 15:08:52 -0400

    Add files via upload

---
+++ Letra N.cpp
@@ -0,0 +1,30 @@
+#include <windows.h>
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+ void gotoxy(int x,int y){  
+      HANDLE hcon;  
+      hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+      COORD dwPos;  
+      dwPos.X = x;  
+      dwPos.Y= y;  
+      SetConsoleCursorPosition(hcon,dwPos);  
+ }  
+ 
+ void dibujarE(int x, int y, int n){
+ 	for (int i=0; i<n; i++){
+ 		for (int j=0; j<n; j++){
+ 			
+ 			if(j == 0 || j == n-1 || (i == j && i>0 && i<n-1)){
+ 				gotoxy (x + j, y + i);
+ 				cout<<"*";
+			 }
+		 }
+	 }
+ }
+ 
+ int main(){  
+	dibujarE(7,5,10);
+      return 0;  
+ }  
